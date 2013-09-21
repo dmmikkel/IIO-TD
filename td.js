aef7b735e772014c3e03226ad784c3c8a2e43999
Dag Martin Mikkelsen <dmi@Dags-MacBook-Pro.local>
2013-09-21 20:46:50 +0200

    Started on tower settings

--- td.js
+++ td.js
@@ -1,4 +1,15 @@
-
+var TOWER_SHOOTER = {
+	"name" : "Shooter",
+	"attackInterval" : 300,
+	"range" : 100,
+	"bullet" : {
+		"slows" : false,
+		"damage" : 10,
+		"speed" : 5,
+		"splash" : false,
+		"splashRange" : 0
+	}
+};
 
 TowerDefence = function(io){
 	var STATE_NONE = 0;
@@ -6,12 +17,16 @@ TowerDefence = function(io){
 
 	var state = STATE_NONE;
 
+	var towerIndicator = new iio.Circle(-100, -100, 16);
+	towerIndicator.setFillStyle('rgba(255,255,255,0.5)');
+	io.addObj(towerIndicator);
+
 	io.addGroup('towers');
 	io.addGroup('enemies');
 	io.addGroup('bullets');
 
-	var grid = new iio.Grid(0, 0, 32, 32, 32);
-	grid.setStrokeStyle('red', 1);
+	var grid = new iio.Grid(0, 0, 20, 20, 32);
+	grid.setStrokeStyle('rgba(0,0,0,0.1)', 1);
 	io.addObj(grid);
 
 	// COLLISION
@@ -32,6 +47,7 @@ TowerDefence = function(io){
 		var enemies = io.getGroup('enemies');
 		var bullets = io.getGroup('bullets');
 
+		// Bullets
 		for (var i = 0; i < towers.length; i++) {
 			for (var e = 0; e < enemies.length; e++) {
 				if (towers[i].pos.distance(enemies[e].pos) <= towers[i].range) {
@@ -41,8 +57,8 @@ TowerDefence = function(io){
 						var bullet = new iio.Circle(towers[i].pos, 2);
 						bullet.setFillStyle('black');
 						bullet.enableKinematics();
-						bullet.damage = towers[i].bulletDamage;
-						bullet.speed = 10;
+						bullet.damage = towers[i].bullet.damage;
+						bullet.speed = towers[i].bullet.speed;;
 						bullet.targetEnemy = enemies[e];
 						var vector = enemies[e].pos.clone().sub(bullet.pos);
 						bullet.setVel(vector.normalize().mult(bullet.speed));
@@ -56,38 +72,50 @@ TowerDefence = function(io){
 		for (var i = 0; i < bullets.length; i++) {
 			if (bullets[i].targetEnemy.health <= 0) {
 				io.rmvObj(bullets[i]);
+			} else {
+				var vector = bullets[i].targetEnemy.pos.clone().sub(bullets[i].pos);
+				bullets[i].setVel(vector.normalize().mult(bullets[i].speed));
 			}
-			var vector = bullets[i].targetEnemy.pos.clone().sub(bullets[i].pos);
-			bullets[i].setVel(vector.normalize().mult(bullets[i].speed));
 		};
 	});
 
 	// ENEMIES
 	io.setFramerate(1, function(){
-		var enemy = new iio.Circle(io.canvas.center.x, 0, 5);
+		var enemy = new iio.Circle(200, 0, 5);
 		enemy.setFillStyle('blue');
 		enemy.enableKinematics();
-		enemy.setVel(0,2);
+		enemy.setVel(0,0.5);
 		enemy.setBound('bottom',io.canvas.height+80);
 		enemy.health = 100;
 		io.addToGroup('enemies', enemy, 2);
 	});
 	
-
+	// MOUSE EVENTS
 	io.canvas.addEventListener('mousedown', function(event){
 		switch (state) {
 			case STATE_PLACING_TOWER:
 				var cell = grid.getCellAt(io.getEventPosition(event));
 				var cellCenter = grid.getCellCenter(cell);
-
 				var tower = new iio.Circle(cellCenter, 16);
+				var towerType = TOWER_SHOOTER;
 				tower.setFillStyle('green');
-				tower.attackInterval = 500;
-				tower.bulletDamage = 30;
-				tower.range = 200;
+
+				tower.attackInterval = towerType.attackInterval;
+				tower.bullet = {};
+				tower.bullet.damage = towerType.bullet.damage;
+				tower.bullet.speed = towerType.bullet.speed;
+				tower.range = towerType.range;
 				tower.lastShot = 0;
 				io.addToGroup('towers', tower, 1);
 				break;
+		}
+	});
+
+	io.canvas.addEventListener('mousemove', function(event){
+		// Tower indicator
+		if (state == STATE_PLACING_TOWER) {
+			var cell = grid.getCellAt(io.getEventPosition(event));
+			towerIndicator.setPos(grid.getCellCenter(cell));
 		}
 	});
 
